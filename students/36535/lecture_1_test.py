0d8e000991f23bd259e985eab70d6ee0f5d3d2e8
samet <sametshn53@gmail.com>
2026-04-02 07:51:18 +0000

    sync all files again

---
+++ students/36535/lecture_1_test.py
@@ -0,0 +1,15 @@
+print("Hello samet, World!")
+a=1
+l = []
+while a<5:
+    if a>2:
+        print(a)
+        l.append(a)
+    a+=1
+print(l)
+
+st = 'Helllo'
+fruits = ['apple', 'orange', 'banana']
+
+for fruit in fruits:
+    print(fruit)
---
+++ students/36535/lecture_2_entry_test.py
@@ -0,0 +1,16 @@
+my_birthday = 12
+
+print(my_birthday)
+print(type(my_birthday))
+
+my_name = "Samet Sahin"
+
+print(my_name)
+print(type(my_name))
+
+my_grade = 4.5
+
+int_grade = int(my_grade)
+
+print(int_grade)
+print(type(int_grade))
