c659430acc46b3bff42ec39904f28162cd6f7116
samet <sametshn53@gmail.com>
2026-04-09 08:29:46 +0000

    to more complex test6

--- students/36535/test_06_class_with_constructor.py
+++ students/36535/test_06_class_with_constructor.py
@@ -5,9 +5,15 @@ class Second_Test:
         self.skin_color = skin_color
 
 person1 = Second_Test("samet", 180, "white")
+person2 = Second_Test("maksym",190,"black")
+
 
 print(person1.name)
 print(person1.height)
 print(person1.skin_color)
+
+print(person2.name)
+print(person2.height)
+print(person2.skin_color)
 
         
--- students/36535/test_06_empty_class.py
+++ students/36535/test_06_empty_class.py
@@ -1,10 +1,17 @@
-class EmptyTest:
+class Empty:
     pass
 
-person1=EmptyTest()
+person1=Empty()
+person2=Empty()
 person1.name="samet"
 person1.age=22
 person1.nationality="turkish"
+
+person2.name="maksym"
+person2.age=23
 print(person1.name)
 print(person1.age)
 print(person1.nationality)
+
+print(person2.name)
+print(person2.age)
