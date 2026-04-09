50e5b400345e5faed8cb04af319051299c8a0b19
samet <sametshn53@gmail.com>
2026-04-09 08:18:45 +0000

    test6

---
+++ students/36535/test_06_class_with_constructor.py
@@ -0,0 +1,13 @@
+class Second_Test:
+    def __init__(self, name, height, skin_color):
+        self.name = name
+        self.height = height
+        self.skin_color = skin_color
+
+person1 = Second_Test("samet", 180, "white")
+
+print(person1.name)
+print(person1.height)
+print(person1.skin_color)
+
+        
---
+++ students/36535/test_06_empty_class.py
@@ -0,0 +1,10 @@
+class EmptyTest:
+    pass
+
+person1=EmptyTest()
+person1.name="samet"
+person1.age=22
+person1.nationality="turkish"
+print(person1.name)
+print(person1.age)
+print(person1.nationality)
